13c21459ec49a3ef10c8c13a16fa08923265d733
Danilo N. B. <danilobenatti@gmail.com>
2023-10-11 14:17:18 -0300

    Configuring the ViewResolver with Thymeleaf.

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,15 +1,60 @@
 package com.algaworks.brewer.config;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.ISpringTemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.algaworks.brewer.controller.BeersController;
 
 @Configuration
 @ComponentScan(basePackageClasses = { BeersController.class })
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurationSupport{
+public class WebConfig extends WebMvcConfigurationSupport
+	implements ApplicationContextAware {
+	
+	private ApplicationContext applicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+	
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
+	
+	@Bean
+	public TemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver
+			.setTemplateEngine((ISpringTemplateEngine) templateEngine());
+		viewResolver.setCharacterEncoding("UTF-8");
+		return viewResolver;
+	}
 	
 }
--- src/main/java/com/algaworks/brewer/controller/BeersController.java
+++ src/main/java/com/algaworks/brewer/controller/BeersController.java
@@ -1,8 +1,14 @@
 package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BeersController {
+	
+	@RequestMapping(path = "/beers/start")
+	public String start() {
+		return "beers/BeersRegistration";
+	}
 	
 }
